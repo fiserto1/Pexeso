21d81bbf3688613392435ac500ef9d45d8c45d58
Tomas <Tomas@Tomas>
2014-04-05 17:15:31 +0200

    zalozena trida pro jednu hru

---
+++ Pexeso/src/pexeso/Game.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pexeso;
+
+/**
+ *
+ * @author Tomas
+ */
+public class Game {
+    private AbstractPlayer player1;
+    private AbstractPlayer player2;
+
+    public Game(AbstractPlayer player1, AbstractPlayer player2) {
+        this.player1 = player1;
+        this.player2 = player2;
+    }
+    
+    public void playGame() {
+        
+    }
+    
+    public void saveGame() {
+        
+    }
+    
+    public void loadGame() {
+        
+    }
+}
