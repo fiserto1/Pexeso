233d39261b85077a706012b234930bf0ecc330f8
Tomas <Tomas@Tomas>
2014-04-06 13:51:18 +0200

    zalozena trida pro jeden tah se dve parametry

--- Pexeso/src/pexeso/OneMove.java
+++ Pexeso/src/pexeso/OneMove.java
@@ -11,5 +11,29 @@ package pexeso;
  * @author Tomas
  */
 public class OneMove {
+    private Card firstCard;
+    private Card secondCard;
+
+    public OneMove(Card firstCard, Card secondCard) {
+        this.firstCard = firstCard;
+        this.secondCard = secondCard;
+    }
+
+    public Card getFirstCard() {
+        return firstCard;
+    }
+
+    public void setFirstCard(Card firstCard) {
+        this.firstCard = firstCard;
+    }
+
+    public Card getSecondCard() {
+        return secondCard;
+    }
+
+    public void setSecondCard(Card secondCard) {
+        this.secondCard = secondCard;
+    }
+    
     
 }
