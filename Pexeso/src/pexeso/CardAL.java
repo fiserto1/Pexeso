4400e2b4635e715774f29350aad08cfd5260f52e
Tomas <Tomas@Tomas>
2014-04-06 19:37:06 +0200

    prepsana metoda pro kliknuti na dvojici karet, pridan getter na vraceni tahu

--- Pexeso/src/pexeso/CardAL.java
+++ Pexeso/src/pexeso/CardAL.java
@@ -6,8 +6,10 @@
 
 package pexeso;
 
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.ImageIcon;
 
 /**
  *
@@ -16,6 +18,9 @@ import java.awt.event.ActionListener;
 public class CardAL implements ActionListener{
 
     private Card card;
+//    static boolean ready;
+    private static Card firstClickedCard;
+    private static Card secondClickedCard;
     
     public CardAL(Card card) {
         this.card = card;
@@ -24,10 +29,28 @@ public class CardAL implements ActionListener{
     @Override
     public void actionPerformed(ActionEvent e) {
         if (card.getIcon() == null) {
-            card.setIcon(card.getCardImage());
-        } else {
-            card.setIcon(null);
+            if (firstClickedCard == null) {
+                card.setText("");
+                Image newImage = card.getCardImage().getImage().getScaledInstance(
+                        card.getCardImage().getIconWidth() / 2, -1, Image.SCALE_SMOOTH);
+                card.setIcon(new ImageIcon(newImage));
+                firstClickedCard = card;
+            }
+            else if (secondClickedCard == null) {
+                card.setText("");
+                Image newImage = card.getCardImage().getImage().getScaledInstance(
+                        card.getCardImage().getIconWidth() / 2, -1, Image.SCALE_SMOOTH);
+                card.setIcon(new ImageIcon(newImage));
+                secondClickedCard = card;
+//                ready = true;
+            }
         }
     }
     
+    public static OneMove getMove() {
+        OneMove move = new OneMove(firstClickedCard, secondClickedCard);
+        firstClickedCard = null;
+        secondClickedCard = null;
+        return move;
+    }
 }
