22b11b55ee66c61a0d31a0e5f9158ff3e0e85fed
Tomas <Tomas@Tomas>
2014-04-23 15:50:33 +0200

    lehce procisteno a zmeneno poradi metod

--- Pexeso/src/pexeso/Game.java
+++ Pexeso/src/pexeso/Game.java
@@ -33,7 +33,7 @@ public class Game implements Serializable, Runnable{
     private boolean playerOnTurn = true;
     private AbstractPlayer player1;
     private AbstractPlayer player2;
-//    private HeadFrame frame;
+
     private DeckOfCards deck;
     private int uncoveredCards = 0;
     private OneMove newMove;
@@ -41,58 +41,6 @@ public class Game implements Serializable, Runnable{
     private transient Message output;
     private boolean endOfGame;
     
-//    private Timer showTimer = new Timer(500, new ActionListener() {
-//
-//        @Override
-//        public void actionPerformed(ActionEvent e) {
-////            System.out.println("ShowTime bezi.");
-//            showTimer.stop();
-//            compareCards();
-//            if (uncoveredCards == DeckOfCards.NUMBER_OF_CARDS) {
-//                endGame();
-//            }
-//            else {
-//                checkTimer.start();
-//            }
-//        }
-//    });
-//    private Timer checkTimer = new Timer(50, new ActionListener() {
-//
-//        @Override
-//        public void actionPerformed(ActionEvent e) {
-////            System.out.println("CheckTimerBezi");
-//            if (playerOnTurn) {
-//                newMove = null;
-//                newMove = player1.move(deck);
-//            }
-//            else {
-//                newMove = null;
-//                newMove = player2.move(deck);
-//            }
-//            if (newMove != null) {
-//                checkTimer.stop();
-//                showTimer.start();
-//            }
-//        }
-//    });
-    
-    
-
-    public void changePlayerOnTurn() {
-        if(playerOnTurn) {
-            playerOnTurn = false;
-            output.setHeadMessage("Player Two's turn.");
-            
-        }
-        else {
-            playerOnTurn = true;
-            output.setHeadMessage("Player One's turn.");
-        }
-    }
-    
-    //COUNTDOWN
-//    private int countdown = 21;
-//    private Timer timer;
 
     public Game(AbstractPlayer player1, AbstractPlayer player2, DeckOfCards deck) {
         this.player1 = player1;
@@ -103,7 +51,8 @@ public class Game implements Serializable, Runnable{
     }
     
     
-    public void playGame() {
+    @Override
+    public void run() {
         player1.setName(player1.name);
         player1.setScore(player1.score);
         player1.setAvatar(player1.avatar);
@@ -112,81 +61,36 @@ public class Game implements Serializable, Runnable{
         player2.setAvatar(player2.avatar);
         if (playerOnTurn) {
             output.setHeadMessage("Player One's turn.");
-        }
-        else {
+        } else {
             output.setHeadMessage("Player Two's turn.");
         }
-//        checkTimer.start();
+
         while (!endOfGame) {
+
             if (playerOnTurn) {
                 newMove = player1.move(deck);
-            }
-            else {
+            } else {
                 newMove = player2.move(deck);
             }
-            
+
+            if (gameInterrupted) {
+                return;
+            }
             //card show
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException ex) {
                 System.out.println("Game cant sleep.");
             }
-            
-            compareCards();
+
+            if (newMove.getFirstCard() != null && newMove.getSecondCard() != null) {
+                compareCards();
+            }
             if (uncoveredCards == DeckOfCards.NUMBER_OF_CARDS) {
                 endGame();
             }
         }
-        
-        //COUNTDOWN
-//        timer = new Timer(1000, new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//                countdown--;
-//                refreshCountDown();
-//                if (countdown == 0) {
-//                    timer.stop();
-//                }
-//            }
-//        });
-//        timer.start();
     }
-    
-//    public void addListernersToTimers() {
-//        checkTimer.addActionListener(new ActionListener() {
-//
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-////                System.out.println("CheckTimerBezi2");
-//                if (playerOnTurn) {
-//                    newMove = null;
-//                    newMove = player1.move(deck);
-//                } else {
-//                    newMove = null;
-//                    newMove = player2.move(deck);
-//                }
-//                if (newMove != null) {
-//                    checkTimer.stop();
-//                    showTimer.start();
-//                }
-//            }
-//        });
-//        
-//        showTimer.addActionListener(new ActionListener() {
-//
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-////                System.out.println("ShowTimerbezi2");
-//                showTimer.stop();
-//                compareCards();
-//                if (uncoveredCards == DeckOfCards.NUMBER_OF_CARDS) {
-//                    endGame();
-//                } else {
-//                    checkTimer.start();
-//                }
-//            }
-//        });
-//    }
     
     public void endGame() {
         if (playerOnTurn) {
@@ -195,42 +99,6 @@ public class Game implements Serializable, Runnable{
             output.setHeadMessage(player2.getName() + " WON!!");
         }
         endOfGame = true;
-    }
-    
-    public void saveGame() {
-//        JFileChooser fileChooser = new JFileChooser();
-//        fileChooser.showSaveDialog(null);
-//        if (fileChooser.getSelectedFile() != null) {
-        
-//            stopAllTimers();
-        
-//            String filepath = fileChooser.getSelectedFile().getAbsolutePath();
-//            System.out.println(filepath);
-            String filepath = System.getProperty("user.dir") + "\\savedGame.txt";
-            ObjectOutputStream objOutStr = null;
-            try {
-                objOutStr = new ObjectOutputStream(new FileOutputStream(filepath));
-                objOutStr.writeObject(this);
-                objOutStr.close();
-                output.setErrorMessage("Save successful.");
-            } catch (FileNotFoundException fnfe) {
-                output.setErrorMessage("File not found.");
-            } catch (IOException ioe) {
-                output.setErrorMessage("IOExp");
-            } finally {
-                playGame();
-                try {
-                    if (objOutStr != null) {
-                        objOutStr.close();
-                    }
-                } catch (IOException ex) {
-                    output.setErrorMessage("Stream not closed.");
-                }
-            }
-//        }
-        
-        
-        
     }
     
     private void compareCards() {
@@ -257,25 +125,16 @@ public class Game implements Serializable, Runnable{
         }
     }
     
-//    public void setScore() {
-//        if (playerOnTurn) {
-//            player1.score += 10;
-//            frame.setPlayerOneScoreLabel("Score: " + player1.score);
-//        } else {
-//            player2.score += 10;
-//            frame.setPlayerTwoScoreLabel("Score: " + player2.score);
-//        }
-//    }
-    
-    //COUNTDOWN
-//    private void refreshCountDown() {
-//        frame.setPlayerOnTurnLabel("Countdown: " + countdown);
-//    }
-    
-//    public void stopAllTimers() {
-//        checkTimer.stop();
-//        showTimer.stop();
-//    }
+    public void changePlayerOnTurn() {
+        if (playerOnTurn) {
+            playerOnTurn = false;
+            output.setHeadMessage("Player Two's turn.");
+
+        } else {
+            playerOnTurn = true;
+            output.setHeadMessage("Player One's turn.");
+        }
+    }
     
     public boolean isPlayerOnTurn() {
         return playerOnTurn;
@@ -291,47 +150,6 @@ public class Game implements Serializable, Runnable{
 
     public void setDeck(DeckOfCards deck) {
         this.deck = deck;
-    }
-
-    @Override
-    public void run() {
-        player1.setName(player1.name);
-        player1.setScore(player1.score);
-        player1.setAvatar(player1.avatar);
-        player2.setName(player2.name);
-        player2.setScore(player2.score);
-        player2.setAvatar(player2.avatar);
-        if (playerOnTurn) {
-            output.setHeadMessage("Player One's turn.");
-        } else {
-            output.setHeadMessage("Player Two's turn.");
-        }
-//        checkTimer.start();
-        while (!endOfGame) {
-            
-            if (playerOnTurn) {
-                newMove = player1.move(deck);
-            } else {
-                newMove = player2.move(deck);
-            }
-
-            if (gameInterrupted) {
-                return;
-            }
-            //card show
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException ex) {
-                System.out.println("Game cant sleep.");
-            }
-
-            if (newMove.getFirstCard() != null && newMove.getSecondCard() != null) {
-                compareCards();
-            }
-            if (uncoveredCards == DeckOfCards.NUMBER_OF_CARDS) {
-                endGame();
-            }
-        }
     }
 
     public AbstractPlayer getPlayer1() {
@@ -357,6 +175,4 @@ public class Game implements Serializable, Runnable{
     public void setOutputDelegate(MessageDelegate delegate) {
         this.output = new Message(delegate);
     }
-    
-    
 }
