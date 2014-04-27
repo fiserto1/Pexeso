97d7e3350dc97fc76398bab1e64b9c69127e81cc
Tomas <Tomas@Tomas>
2014-04-27 02:22:36 +0200

    rozrezano do mensich metod

--- Pexeso/src/pexeso/players/ComputerPlayer.java
+++ Pexeso/src/pexeso/players/ComputerPlayer.java
@@ -31,77 +31,11 @@ public class ComputerPlayer extends AbstractPlayer {
     
     @Override
     public OneMove move(OneMove myLastMove, ArrayList<OneMove> oppMoves) {
-        if (myLastMove != null) {
-            if (myLastMove.getFirstCardCompareNumber() != -1) {
-                if (correctMoves.containsKey(myLastMove.getFirstCardCompareNumber())) {
-                    ArrayList<Integer> newCard = correctMoves.get(myLastMove.getFirstCardCompareNumber());
-                    if (newCard.size() == 1 && newCard.get(0) != myLastMove.getFirstCardIDNumber()) {
-                        newCard.add(myLastMove.getFirstCardIDNumber());
-                    }
-                } else {
-                    ArrayList<Integer> newCard = new ArrayList<Integer>();
-                    newCard.add(myLastMove.getFirstCardIDNumber());
-                    correctMoves.put(myLastMove.getFirstCardCompareNumber(), newCard);
-                }
-            }
-            if (myLastMove.getSecondCardCompareNumber() != -1) {
-                if (correctMoves.containsKey(myLastMove.getSecondCardCompareNumber())) {
-                    ArrayList<Integer> newCard = correctMoves.get(myLastMove.getSecondCardCompareNumber());
-                    if (newCard.size() == 1 && newCard.get(0) != myLastMove.getSecondCardIDNumber()) {
-                        newCard.add(myLastMove.getSecondCardIDNumber());
-                    }
-                } else {
-                    ArrayList<Integer> newCard = new ArrayList<Integer>();
-                    newCard.add(myLastMove.getSecondCardIDNumber());
-                    correctMoves.put(myLastMove.getSecondCardCompareNumber(), newCard);
-                }
-            }
-            
-            //delete correct moves
-            if (myLastMove.getFirstCardCompareNumber() == myLastMove.getSecondCardCompareNumber()) {
-                if (correctMoves.containsKey(myLastMove.getFirstCardCompareNumber())) {
-                    correctMoves.remove(myLastMove.getFirstCardCompareNumber());
-                    uncoveredCards.add(myLastMove.getFirstCardIDNumber());
-                    uncoveredCards.add(myLastMove.getSecondCardIDNumber());
-                }
-            }
-        }
+        saveToMemory(myLastMove);
         
         if (oppMoves != null) {
             for (int i = 0; i < oppMoves.size(); i++) {
-                if (oppMoves.get(i).getFirstCardCompareNumber() != -1) {
-                    if (correctMoves.containsKey(oppMoves.get(i).getFirstCardCompareNumber())) {
-                        ArrayList<Integer> newCard = correctMoves.get(oppMoves.get(i).getFirstCardCompareNumber());
-                        if (newCard.size() == 1 && newCard.get(0) != oppMoves.get(i).getFirstCardIDNumber()) {
-                            newCard.add(oppMoves.get(i).getFirstCardIDNumber());
-                        }
-                    } else {
-                        ArrayList<Integer> newCard = new ArrayList<Integer>();
-                        newCard.add(oppMoves.get(i).getFirstCardIDNumber());
-                        correctMoves.put(oppMoves.get(i).getFirstCardCompareNumber(), newCard);
-                    }
-                }
-                if (oppMoves.get(i).getSecondCardCompareNumber() != -1) {
-                    if (correctMoves.containsKey(oppMoves.get(i).getSecondCardCompareNumber())) {
-                        ArrayList<Integer> newCard = correctMoves.get(oppMoves.get(i).getSecondCardCompareNumber());
-                        if (newCard.size() == 1 && newCard.get(0) != oppMoves.get(i).getSecondCardIDNumber()) {
-                            newCard.add(oppMoves.get(i).getSecondCardIDNumber());
-                        }
-                    } else {
-                        ArrayList<Integer> newCard = new ArrayList<Integer>();
-                        newCard.add(oppMoves.get(i).getSecondCardIDNumber());
-                        correctMoves.put(oppMoves.get(i).getSecondCardCompareNumber(), newCard);
-                    }
-                }
-
-                //delete correct moves
-                if (oppMoves.get(i).getFirstCardCompareNumber() == oppMoves.get(i).getSecondCardCompareNumber()) {
-                    if (correctMoves.containsKey(oppMoves.get(i).getFirstCardCompareNumber())) {
-                        correctMoves.remove(oppMoves.get(i).getFirstCardCompareNumber());
-                        uncoveredCards.add(oppMoves.get(i).getFirstCardIDNumber());
-                        uncoveredCards.add(oppMoves.get(i).getSecondCardIDNumber());
-                    }
-                }
+                saveToMemory(oppMoves.get(i));
             }
         }
         
@@ -111,6 +45,11 @@ public class ComputerPlayer extends AbstractPlayer {
                 return new OneMove(entry.getValue().get(0), entry.getValue().get(1));
             }
         }
+        
+        return playRandom();
+    }
+    
+    private OneMove playRandom() {
         Random rnd = new Random();
         int firstTurn;
         int secondTurn;
@@ -124,11 +63,41 @@ public class ComputerPlayer extends AbstractPlayer {
             if (uncoveredCards.contains(firstTurn) || uncoveredCards.contains(secondTurn)) {
                 continue;
             }
-            
+
 //            System.out.println("playing random: " + firstTurn + "," + secondTurn);
             return new OneMove(firstTurn, secondTurn);
         }
-        
-        return new OneMove(0, 1);
+        return null;
+    }
+
+    private void saveToMemory(OneMove move) {
+        if (move != null) {
+            saveToMap(move.getFirstCardCompareNumber(), move.getFirstCardIDNumber());
+            saveToMap(move.getSecondCardCompareNumber(), move.getSecondCardIDNumber());
+
+            //delete correct moves
+            if (move.getFirstCardCompareNumber() == move.getSecondCardCompareNumber()) {
+                if (correctMoves.containsKey(move.getFirstCardCompareNumber())) {
+                    correctMoves.remove(move.getFirstCardCompareNumber());
+                    uncoveredCards.add(move.getFirstCardIDNumber());
+                    uncoveredCards.add(move.getSecondCardIDNumber());
+                }
+            }
+        }
+    }
+
+    private void saveToMap(int compareNumber, int idNumber) {
+        if (compareNumber != -1) {
+            if (correctMoves.containsKey(compareNumber)) {
+                ArrayList<Integer> newCard = correctMoves.get(compareNumber);
+                if (newCard.size() == 1 && newCard.get(0) != idNumber) {
+                    newCard.add(idNumber);
+                }
+            } else {
+                ArrayList<Integer> newCard = new ArrayList<Integer>();
+                newCard.add(idNumber);
+                correctMoves.put(compareNumber, newCard);
+            }
+        }
     }
 }
