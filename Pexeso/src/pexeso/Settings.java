1883c0437677af4c8d5d75d0492ada0988b6bc20
Tomas <Tomas@Tomas>
2014-05-04 20:22:01 +0200

    zalozena trida pro nastaveni hry

---
+++ Pexeso/src/pexeso/Settings.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pexeso;
+
+/**
+ *
+ * @author Tomas
+ */
+public class Settings {
+    private int numberOfCards;
+    private int difficulty;
+
+    public Settings(int numberOfCards, int difficulty) {
+        this.numberOfCards = numberOfCards;
+        this.difficulty = difficulty;
+    }
+
+    public int getNumberOfCards() {
+        return numberOfCards;
+    }
+
+    public void setNumberOfCards(int numberOfCards) {
+        this.numberOfCards = numberOfCards;
+    }
+
+    public int getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(int difficulty) {
+        this.difficulty = difficulty;
+    }
+}
